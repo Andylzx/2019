3d74d44174e20c5fc25635dc21632bd8c422bb79
Andylzx <986249154@qq.com>
2019-05-31 15:24:55 +0800

    完整版

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
+    //初始化轮播图，定时2S自动滑动
     $('.carousel').carousel({
         interval: 2000,
-    });//初始化轮播图，定时2S自动滑动
+    });
     var startX, endX;//声明触摸的两个变量
     var offset = 30;//声明触摸距离的变量
     $('.carousel-inner').on('touchstart', function (e) {
@@ -21,13 +22,17 @@ $(document).ready(function () {
             }
         }
     });
+
+    //鼠标进入微信图标的时候显示二维码，离开的时候隐藏二维码
     $("#we").parent().find("img").hide();//二维码默认为隐藏
     $("#we").mouseenter(function () {
         $(this).parent().find("img").show()
     }).mouseleave(function () {
         $(this).parent().find("img").hide()
-    });//鼠标进入微信图标的时候显示二维码，离开的时候隐藏二维码
+    });
     $(".top").hide();//返回顶部按钮默认隐藏
+
+    //给浏览器注册滚动事件，监听卷曲出去的距离
     $(window).scroll(function () {
         var scrollY=$(window).scrollTop();//卷曲出去的距离
         var height=$("header").height()+$(".banner").height();//头部加轮播图的高度
@@ -36,10 +41,106 @@ $(document).ready(function () {
         }else{
             $(".top").hide()//当卷曲的距离小于头部加轮播图高度时显示按钮
         }
-    });//给浏览器注册滚动事件，监听卷曲出去的距离
+    });
+
+    //给按钮注册点击事件，点击按钮的时候卷曲的距离变为0，页面滚回顶部
     $(".top").click(function () {
         $("html,body").stop().animate({scrollTop:0},500);
-        // $(window).scrollTop(0);
-    });//给按钮注册点击事件，点击按钮的时候卷曲的距离变为0，页面滚回顶部
+    });
 
+    //给关闭按钮定义一个显示主页，隐藏大图的方法
+    close=function () {
+        $(".render").css("display","none").empty();
+        $(".mask").css("display","none");
+    };
+
+    //点击关于人物的小图，从json文本里请求数据并渲染到页面中
+   $("#people").find("a").click(function () {
+        var id=$(this).index();//定义一个变量获取点击的a标签的索引值
+       $(".mask").fadeIn(200);
+       $(".render").fadeIn(200);//点击小图的时候遮罩和页面主体显示出来
+       $.ajax({
+           url:"js/per.json",//人物的数据
+           data:"",
+           dataType:"json",
+           type:"get",
+           success:function (data) {
+               var img=template("template",{list:data,index:id});//将数据和索引值传入
+               $(".render").prepend(img);//将渲染的数据添加到render中
+           }
+       });
+       var index=0;//初始化索引值
+       left=function () {
+           var width=$(".render_li").width();//获取每张轮播图的宽度
+           var length=parseInt($(".render_ul").width()/$(".render_li").width());//获取照片的张数
+           if(index==0){//判断索引为0，则索引变为最后一个数字
+               index=length;
+           }
+           index--;//减1索引值对应最后一张图片
+           $(".render_ul").stop().animate({marginLeft:"-"+(width*index)+"px"},500);
+       };//右滑动时marginleft为负数
+       right=function () {
+           var width=$(".render_li").width();//获取每张轮播图的宽度
+           var length=parseInt($(".render_ul").width()/$(".render_li").width());//获取照片的张数
+           if(index==length-1){//判断索引对应最后一张图片时，索引值设置为-1
+               index=-1;
+           }
+           index++;
+           // $(".render_ul").css({'transform':'translateX(-'+width+'px)'});
+           $(".render_ul").stop().animate({marginLeft:"-"+(width*index)+"px"},500);
+       }
+   });
+
+//点击关于旅游的小图，从json文本里请求数据并渲染到页面中
+    $("#travel").find("a").click(function () {
+        var id=$(this).index();
+        $(".mask").fadeIn(200);
+        $(".render").fadeIn(200);
+        $.ajax({
+            url:"js/travel.json",//旅行的数据
+            data:"",
+            dataType:"json",
+            type:"get",
+            success:function (data) {
+                var img=template("template",{list:data,index:id});
+                $(".render").prepend(img);
+            }
+        });
+        var index=0;
+        left=function () {
+            var width=$(".render_li").width();
+            var length=parseInt($(".render_ul").width()/$(".render_li").width());
+            if(index==0){
+                index=length;
+            }
+            index--;
+            $(".render_ul").stop().animate({marginLeft:"-"+(width*index)+"px"},500);
+        };
+        right=function () {
+            var width=$(".render_li").width();
+            var length=parseInt($(".render_ul").width()/$(".render_li").width());
+            if(index==length-1){
+                index=-1;
+            }
+            index++;
+            $(".render_ul").stop().animate({marginLeft:"-"+(width*index)+"px"},500);
+        }
+    });
+
+//点击一风一景的小图，从json文本里请求数据并渲染到页面中
+    $("#scenery").find("a").click(function () {
+        var id=$(this).index();
+        $(".mask").fadeIn(200);
+        $(".render").fadeIn(200);
+        $.ajax({
+            url:"js/scenery.json",//风景的数据
+            data:"",
+            dataType:"json",
+            type:"get",
+            success:function (data) {
+                var img=template("template-scenery",{list:data,index:id});
+                $(".render").prepend(img);
+            }
+        });
+    });
 });
